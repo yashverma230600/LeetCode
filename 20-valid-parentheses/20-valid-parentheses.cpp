402c1f926fa5d09e5690bd6571d67591b432e162
Yash Verma <69080521+yashverma230600@users.noreply.github.com>
2022-05-25 22:32:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.87%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack<char> st;
+        int length  = s.size();
         
-        stack<char> stk;
-        
-        for(auto bracket : s)
-        {
-            if(bracket== '(' || bracket == '{' || bracket == '[')
-            {
-                stk.push(bracket);
+        for(int i=0;i<length;i++){
+            if(s[i] =='(' || s[i] =='{' || s[i] =='[')
+                st.push(s[i]);
+            else if(s[i] ==')' || s[i] =='}' || s[i] ==']'){
+                if(st.empty())
+                    return false;
+                
+                else if(s[i] == ')' && st.top()!='(')
+                    return false;
+                else if(s[i] == '}' && st.top()!='{')
+                    return false;
+                else if(s[i] == ']' && st.top()!='[')
+                    return false;
+                
+                else 
+                    st.pop();
+            }
             }
             
-            else
-            {
-                if(stk.empty()  || 
-                   bracket==')' && stk.top()!='(' || 
-                   bracket==']' && stk.top()!='[' || 
-                   bracket=='}' && stk.top()!='{'
-                  )
-                {
-                       return false;
-                }
-                else
-                {
-                    stk.pop();
-                }
-            }
-        }
         
-        return stk.empty();
+                                                   if(st.empty())
+                                                   return true;
+            else
+                                                   return false;
+        
+        
         
     }
 };
