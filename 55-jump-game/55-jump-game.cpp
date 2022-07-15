834c189c8b947f2a684b1ea80cddf5c296349972
Yash Verma <69080521+yashverma230600@users.noreply.github.com>
2022-07-15 11:34:36 +0530

    Time: 111 ms (48.48%), Space: 48.5 MB (43.19%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,0);
+        int n=nums.size(); 
+        int reachable=0;
         
-        int lstone = n-1;
-        for(int i=n-1; i>=0; --i) {
-            if(i==n-1)
-                dp[i] = 1;
-            else {
-                int len = lstone - i;
-                if(len<=nums[i]) {
-                    dp[i] = 1;
-                    lstone = i;
-                } else dp[i] = 0;
+        for(int i=0;i<n;i++){
+            if(reachable<i){
+                return false;}
+            else{
+            reachable=max(reachable,i+nums[i]);
+                
             }
+            
+            
         }
-        return dp[0];
+        return true;
     }
 };
