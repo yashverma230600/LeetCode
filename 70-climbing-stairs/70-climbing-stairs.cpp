013e179f0665269eac019190c14ece6f7d7c7f89
Yash Verma <69080521+yashverma230600@users.noreply.github.com>
2022-07-24 00:47:03 +0530

    Time: 4 ms (22.52%), Space: 6 MB (56.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
-         //Edge cases
-    if(n == 0) return 1;
-    if(n == 1) return 1;
-
-    int *ans = new int [n+1];  //Create an array to store the values of overlapping subproblems
-    ans[0] = ans[1] = 1;       //Fill the independent values in the array
- 
-    for(int i=2; i<=n; i++)    //Now fill the dependent vales in the array
-        ans[i] = ans[i-1] + ans[i-2];
-    return ans[n];
+        int dp[47];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
         
     }
 };
