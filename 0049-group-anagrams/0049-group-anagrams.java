aad4b3571226b8b79c4be1961114e3aeac4ede44
Yash Verma <69080521+yashverma230600@users.noreply.github.com>
2026-05-12 20:09:23 +0530

    Time: 7 ms (73.77%), Space: 49.2 MB (95.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map = new HashMap<>();
+
         for(String s:strs){
             char[] c = s.toCharArray();
             Arrays.sort(c);
@@ -8,11 +9,9 @@ class Solution {
 
             if(!map.containsKey(key)){
                 map.put(key,new ArrayList<>());
+
             }
             map.get(key).add(s);
-
-
-
                     }
                     return new ArrayList<>(map.values());
     }
