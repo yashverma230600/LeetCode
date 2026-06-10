0f6b935107c92d07d631fd5472fa35e8930f54ce
Yash Verma <69080521+yashverma230600@users.noreply.github.com>
2026-06-10 14:46:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,30 +1,29 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length<=1) return intervals;
-
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
+                if(intervals.length<=1) return intervals;
 
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        int pStart=intervals[0][0];
-        int pEnd=intervals[0][1];
-
+        int Ps=intervals[0][0];
+        int Pe=intervals[0][1];
         for(int i=0;i<intervals.length;i++){
-            if(intervals[i][0]<=pEnd){
-                pEnd=Math.max(pEnd,intervals[i][1]);
+            if(intervals[i][0]<=Pe){
+                Pe=Math.max(Pe,intervals[i][0]);
             }
             else{
-                result.add(Arrays.asList(pStart,pEnd));
-                pStart=intervals[i][0];
-                pEnd=intervals[i][1];
+                result.add(Arrays.asList(Ps,Pe));
+                Ps=intervals[i][0];
+                Pe=intervals[i][1];
             }
         }
-        result.add(Arrays.asList(pStart,pEnd));
+        result.add(Arrays.asList(Ps,Pe));
         int[][] ans = new int[result.size()][2];
         for(int i=0;i<result.size();i++){
             ans[i][0] = result.get(i).get(0);
             ans[i][1] = result.get(i).get(1);
         }
-        return ans;
+
+return ans;
 
     }
 }
