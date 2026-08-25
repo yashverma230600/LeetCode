095a7ae8d989f125a6772fea5a65f60a2061a2c8
Yash Verma <69080521+yashverma230600@users.noreply.github.com>
2026-08-25 23:57:05 +0530

    Time: 0 ms (100%), Space: 44.6 MB (52.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,47 +1,36 @@
 class Solution {
-
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int index = -1;
+        int n=nums.length;
+        int index=-1;
 
-        // 1️⃣ Find first decreasing element from right
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
                 break;
             }
         }
-
-        // 2️⃣ If no such index, reverse whole array
-        if (index == -1) {
-            reverse(nums, 0, n - 1);
+        if(index==-1){
+            reverse(nums,0,n-1);
             return;
         }
-
-        // 3️⃣ Find element just greater than nums[index]
-        for (int i = n - 1; i > index; i--) {
-            if (nums[i] > nums[index]) {
-                swap(nums, i, index);
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[index]){
+                swap(nums,i,index);
                 break;
             }
         }
-
-        // 4️⃣ Reverse the suffix
-        reverse(nums, index + 1, n - 1);
+        reverse(nums,index+1,n-1);
     }
-
-    // helper methods
-    void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
-
-    void reverse(int[] nums, int l, int r) {
-        while (l < r) {
-            swap(nums, l, r);
-            l++;
-            r--;
+    private void reverse(int nums[],int start,int end){
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
         }
+    }
+    private void swap(int[] nums, int l,int r){
+        int temp=nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
     }
 }
